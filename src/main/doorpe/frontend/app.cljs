f603c3627a021ba663020d457838b5f9481a7584
Mustafa Basit <mailtobasit74@gmail.com>
2020-08-19 02:34:58 +0530

    from console msg to DOM(hello dom) - initial app setup done

--- src/main/doorpe/frontend/app.cljs
+++ src/main/doorpe/frontend/app.cljs
@@ -1,5 +1,17 @@
-(ns doorpe.frontend.app)
+(ns doorpe.frontend.app
+  (:require [reagent.dom :as reagent]))
 
-(defn init
+(defn app 
   []
-  (println "Hello World"))
+  [:h3 {:style {:color :tomato
+                :font "normal 40px sans-serif"}}
+   "Hello DOM"])
+
+(defn ^:dev/after-load start
+  []
+  (reagent/render [app]
+            (.getElementById js/document "root")))
+
+(defn ^:export init
+  []
+  (start))
