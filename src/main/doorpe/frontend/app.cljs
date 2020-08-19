a747519d3be5a5e0e329a5768e71d41e1b57b751
Mustafa Basit <mailtobasit74@gmail.com>
2020-08-19 23:04:38 +0530

    Switched to material-ui, Added few components examples

--- src/main/doorpe/frontend/app.cljs
+++ src/main/doorpe/frontend/app.cljs
@@ -1,11 +1,55 @@
 (ns doorpe.frontend.app
-  (:require [reagent.dom :as reagent]))
+  (:require [reagent.dom :as reagent]
+            [reagent.core]
+            ["@material-ui/core" :refer [Button Checkbox FormControlLabel TextField]]
+            ["@material-ui/pickers" :refer [DatePicker]]))
 
-(defn app 
+(defn greet
+  [name]
+  (js/alert "welcome " name))
+
+(defn two-br
   []
-  [:h3 {:style {:color :tomato
-                :font "normal 40px sans-serif"}}
-   "Hello DOM"])
+  [:<>
+   [:br]
+   [:br]])
+
+(defn title
+  "returns a 'p' tag with a custom message in it + few new-lines"
+  [msg]
+  [:<>
+   [two-br]
+   [two-br]
+   [:p [:b msg]]])
+
+
+(defn app
+  []
+  [:<>
+   [:p [:b "Basic Button"]]
+   [:> Button {:variant "contained"
+               :color "primary"
+               :on-click #(js/alert "clicked")}
+    "click me"]
+
+   [title "Checkboxes"]
+   [:> Checkbox]
+   [:> Checkbox {:disabled "true"}]
+   [:> Checkbox {:checked true
+                 :color "primary"}]
+   
+  
+   [:> FormControlLabel {:control (reagent.core/as-element [:> Checkbox])
+                         :label "Checkbox with label control"}] 
+   
+   [title "Text Boxes"]
+   [:> TextField {:label "standard"}]
+   
+   [two-br]
+   [:> TextField {:variant "outlined"
+                  :label "outlined"}]
+   
+   [two-br]])
 
 (defn ^:dev/after-load start
   []
